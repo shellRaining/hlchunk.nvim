b909596028964bb5add68db9f486bc894539821e
shell-Raining <shellRaining@gmail.com>
2023-02-21 16:55:27 +0800

    ref: delete some useless code and refactor render to a split func

--- lua/hlchunk/hlchunk.lua
+++ lua/hlchunk/hlchunk.lua
@@ -5,32 +5,8 @@ local M = {}
 
 local ns_id = -1
 
--- set new virtual text to the right place
-function M.hl_cur_chunk()
-    if not opts.config.enabled then
-        return
-    end
-
-    M.clear_hl_chunk()
-    ns_id = vim.api.nvim_create_namespace("hlchunk")
-
-    -- determined the row where parentheses are
-    local beg_row, end_row
-    local pair_pos = utils.get_pair_rows()
-    beg_row = pair_pos[1]
-    end_row = pair_pos[2]
-    if beg_row == end_row then
-        return
-    end
-
-    -- determined the start_col to draw virt_text
-    local shift_width = vim.o.shiftwidth
-    local space_tab = (" "):rep(shift_width)
-    local beg_blank_val = tostring(vim.fn.getline(beg_row)):match("%s*"):gsub("\t", space_tab)
-    local end_blank_val = tostring(vim.fn.getline(end_row)):match("%s*"):gsub("\t", space_tab)
-    local beg_blank_len = #beg_blank_val
-    local end_blank_len = #end_blank_val
-    local start_col = math.min(beg_blank_len, end_blank_len) - shift_width
+local function render_cur_chunk(render_params)
+    local beg_row, end_row, start_col, beg_blank_len, end_blank_len = unpack(render_params)
 
     local row_opts = {
         virt_text_pos = "overlay",
@@ -53,11 +29,28 @@ function M.hl_cur_chunk()
 
     -- render middle section
     for i = beg_row + 1, end_row - 1 do
-        local mid_virt_text = opts.config.hl_chars.vertical_line
-        row_opts.virt_text = { { mid_virt_text, "HLChunkStyle" } }
+        row_opts.virt_text = { { opts.config.hl_chars.vertical_line, "HLChunkStyle" } }
         row_opts.virt_text_win_col = math.max(0, start_col)
         vim.api.nvim_buf_set_extmark(0, ns_id, i - 1, 0, row_opts)
     end
+end
+
+-- set new virtual text to the right place
+function M.hl_cur_chunk()
+    if not opts.config.enabled then
+        return
+    end
+
+    M.clear_hl_chunk()
+    ns_id = vim.api.nvim_create_namespace("hlchunk")
+
+    -- determined the row where parentheses are
+    local beg_row, end_row = unpack(utils.get_pair_rows())
+    if beg_row == end_row then
+        return
+    end
+
+    render_cur_chunk(utils.get_render_params(beg_row, end_row))
 end
 
 -- clear the virtual text marked before
--- lua/hlchunk/utils.lua
+++ lua/hlchunk/utils.lua
@@ -3,14 +3,32 @@ local M = {}
 function M.get_pair_rows()
     local beg_row, end_row
     local base_flag = "nWz"
-    local cur_row_val = vim.fn.getline('.')
-    local cur_col = vim.fn.col('.')
+    local cur_row_val = vim.fn.getline(".")
+    local cur_col = vim.fn.col(".")
     local cur_char = string.sub(cur_row_val, cur_col, cur_col)
 
     beg_row = vim.fn.searchpair("{", "", "}", base_flag .. "b" .. (cur_char == "{" and "c" or ""))
     end_row = vim.fn.searchpair("{", "", "}", base_flag .. (cur_char == "}" and "c" or ""))
 
     return { beg_row, end_row }
+end
+
+function M.get_render_params(beg_row, end_row)
+    local shift_width = vim.o.shiftwidth
+    local space_tab = (" "):rep(shift_width)
+    local beg_blank_val = tostring(vim.fn.getline(beg_row)):match("%s*"):gsub("\t", space_tab)
+    local end_blank_val = tostring(vim.fn.getline(end_row)):match("%s*"):gsub("\t", space_tab)
+    local beg_blank_len = #beg_blank_val
+    local end_blank_len = #end_blank_val
+    local start_col = math.min(beg_blank_len, end_blank_len) - shift_width
+
+    return {
+        beg_row,
+        end_row,
+        start_col,
+        beg_blank_len,
+        end_blank_len,
+    }
 end
 
 return M
