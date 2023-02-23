773e09a3e9772d8954395c524b9650ec2e6929ae
shell-Raining <shellRaining@gmail.com>
2023-02-23 22:00:06 +0800

    feat: add num line highlight support

--- lua/hlchunk/hl_chunk.lua
+++ lua/hlchunk/hl_chunk.lua
@@ -1,5 +1,8 @@
+---@diagnostic disable: unused-local
 local utils = require("hlchunk.utils")
 local opts = require("hlchunk.options")
+local fn = vim.fn
+local api = vim.api
 
 local M = {}
 
@@ -22,17 +25,32 @@ local function render_cur_chunk(render_params)
             .. opts.config.hl_chunk.chars.horizontal_line:rep(end_blank_len - start_col - 2)
             .. opts.config.hl_chunk.chars.right_arrow
 
-        row_opts.virt_text = { { beg_virt_text, "HLChunkStyle" } }
-        vim.api.nvim_buf_set_extmark(0, ns_id, beg_row - 1, 0, row_opts)
-        row_opts.virt_text = { { end_virt_text, "HLChunkStyle" } }
-        vim.api.nvim_buf_set_extmark(0, ns_id, end_row - 1, 0, row_opts)
+        row_opts.virt_text = { { beg_virt_text, "HLChunkStyle1" } }
+        api.nvim_buf_set_extmark(0, ns_id, beg_row - 1, 0, row_opts)
+        row_opts.virt_text = { { end_virt_text, "HLChunkStyle1" } }
+        api.nvim_buf_set_extmark(0, ns_id, end_row - 1, 0, row_opts)
     end
 
     -- render middle section
     for i = beg_row + 1, end_row - 1 do
-        row_opts.virt_text = { { opts.config.hl_chunk.chars.vertical_line, "HLChunkStyle" } }
+        row_opts.virt_text = { { opts.config.hl_chunk.chars.vertical_line, "HLChunkStyle1" } }
         row_opts.virt_text_win_col = math.max(0, start_col)
-        vim.api.nvim_buf_set_extmark(0, ns_id, i - 1, 0, row_opts)
+        api.nvim_buf_set_extmark(0, ns_id, i - 1, 0, row_opts)
+    end
+end
+
+local function render_num_line(beg_row, end_row)
+    fn.sign_unplace("LineNumberGroup", {
+        buffer = fn.bufname("%"),
+    })
+
+    if beg_row >= end_row then
+        return
+    end
+    for i = beg_row, end_row do
+        fn.sign_place("", "LineNumberGroup", "LineNumberInterval", fn.bufname("%"), {
+            lnum = i,
+        })
     end
 end
 
@@ -43,21 +61,23 @@ function M.hl_cur_chunk()
     end
 
     M.clear_hl_chunk()
-    ns_id = vim.api.nvim_create_namespace("hlchunk")
+    ns_id = api.nvim_create_namespace("hlchunk")
 
     -- determined the row where parentheses are
     local beg_row, end_row = unpack(utils.get_pair_rows())
-    if beg_row == end_row then
-        return
+    if beg_row < end_row then
+        render_cur_chunk(utils.get_render_chunk_params(beg_row, end_row))
     end
 
-    render_cur_chunk(utils.get_render_chunk_params(beg_row, end_row))
+    if opts.config.hl_chunk.enable_hl_line_num then
+        render_num_line(beg_row, end_row)
+    end
 end
 
 -- clear the virtual text marked before
 function M.clear_hl_chunk()
     if ns_id ~= -1 then
-        vim.api.nvim_buf_clear_namespace(0, ns_id, 0, -1)
+        api.nvim_buf_clear_namespace(0, ns_id, 0, -1)
     end
 end
 
