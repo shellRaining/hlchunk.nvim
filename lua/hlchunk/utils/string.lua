3ae8c52d611d6a59e3fb56e97a76bd99db3ac8ed
shell-Raining <shellRaining@gmail.com>
2023-03-07 14:05:10 +0800

    delete useless lib

---
+++ lua/hlchunk/utils/string.lua
@@ -0,0 +1,18 @@
+local stringx = {}
+
+function stringx.split(s, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t = {}
+    for str in string.gmatch(s, "([^" .. sep .. "]+)") do
+        table.insert(t, str)
+    end
+    return t
+end
+
+function stringx.firstToUpper(s)
+    return (s:gsub("^%l", string.upper))
+end
+
+return stringx
--- lua/hlchunk/utils/table.lua
+++ lua/hlchunk/utils/table.lua
@@ -1,17 +1,5 @@
 local tablex = {}
 
---- copy a table into another, in-place.
--- @within Copying
--- @tab t1 destination table
--- @tab t2 source (actually any iterable object)
--- @return first table
-function tablex.update(t1, t2)
-    for k, v in pairs(t2) do
-        t1[k] = v
-    end
-    return t1
-end
-
 --- total number of elements in this table.
 -- Note that this is distinct from `#t`, which is the number
 -- of values in the array part; this value will always
