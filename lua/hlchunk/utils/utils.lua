820d94ddf9fe12f0edd15d543e21bcb6e39aaab5
shell-Raining <shellRaining@gmail.com>
2023-03-11 15:08:04 +0800

    fix: fix chunk bug

--- lua/hlchunk/utils/utils.lua
+++ lua/hlchunk/utils/utils.lua
@@ -1,11 +1,11 @@
 ---@diagnostic disable: param-type-mismatch
 local M = {}
 
-function M.get_pair_rows()
+function M.get_chunk_range()
     local beg_row, end_row
     local base_flag = "nWz"
     local cur_row_val = vim.fn.getline(".")
-    local cur_col = vim.fn.col(".")
+    local cur_col = FN.col('.')
     local cur_char = string.sub(cur_row_val, cur_col, cur_col)
 
     beg_row = vim.fn.searchpair("{", "", "}", base_flag .. "b" .. (cur_char == "{" and "c" or ""))
