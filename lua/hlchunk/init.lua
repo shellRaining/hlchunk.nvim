5522d14ea5690c315800198ebbb164e59a5b1c4b
shellRaining <shellRaining@gmail.com>
2023-04-14 23:08:23 +0800

    text: add annotations

--- lua/hlchunk/init.lua
+++ lua/hlchunk/init.lua
@@ -1,13 +1,26 @@
-local hlchunk = {}
+---@class ModOptions
+---@field enable? boolean
+---@field style? table
+---@field chars? table
+---@field use_treesitter? boolean
+---@field exclude_filetype? table
+---@field support_filetype? table
 
--- global var that represents utils and built-in functions
+---@class PlugConfig
+---@field chunk? ModOptions
+---@field line_num? ModOptions
+---@field indent? ModOptions
+---@field blank? ModOptions
+---@field context? ModOptions
+
+-- TODO: add package annotations
+local hlchunk = {}
 local api = vim.api
 
-
 -- get the status(whether enabled) of mods, return a table, the first key is the mod name, the second key is a bool variables to represent whether enabled
----@param params table
+---@param plugin_config PlugConfig
 ---@return table<string, boolean>
-local function get_mods_status(params)
+local function get_mods_status(plugin_config)
     local mods_status = {
         chunk = true,
         line_num = true,
@@ -16,7 +29,7 @@ local function get_mods_status(params)
         context = false,
     }
 
-    for mod_name, mod_conf in pairs(params) do
+    for mod_name, mod_conf in pairs(plugin_config) do
         if mod_conf and mod_conf.enable ~= nil then
             mods_status[mod_name] = mod_conf.enable
         end
@@ -25,8 +38,10 @@ local function get_mods_status(params)
     return mods_status
 end
 
+-- set user commands to enable/disable all mods for those which has been enabled in config file or default config
+---@param mods_status table<string, boolean>
 local function set_usercmds(mods_status)
-    api.nvim_create_user_command("EnableHL", function ()
+    api.nvim_create_user_command("EnableHL", function()
         for mod_name, enabled in pairs(mods_status) do
             if enabled then
                 require("hlchunk.mods." .. mod_name):enable()
@@ -42,12 +57,6 @@ local function set_usercmds(mods_status)
     end, {})
 end
 
----@class PlugConfig
----@field chunk? table
----@field line_num? table
----@field indent? table
----@field blank? table
----@field context? table
 ---@param params PlugConfig
 hlchunk.setup = function(params)
     local mods_status = get_mods_status(params)
