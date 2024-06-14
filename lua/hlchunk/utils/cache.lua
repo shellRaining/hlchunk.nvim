3f0227b57ac05aa5f4502dad6f45474412053154
shellRaining <shellRaining@gmail.com>
2024-06-14 09:31:45 +0800

    feat: indent add mark cache
    ref: add lazy opts
    
    fix: when scroll horizon not handle win_col properly

--- lua/hlchunk/utils/cache.lua
+++ lua/hlchunk/utils/cache.lua
@@ -1,42 +1,69 @@
 local class = require("hlchunk.utils.class")
 
 ---@class Cache
----@field private cache table<number, table<string|number, any>>
----@overload fun():Cache
-local Cache = class(function(self)
+---@field private cache table
+---@field private keys table
+---@overload fun(...):Cache
+local Cache = class(function(self, ...)
+    self.keys = { ... } -- 在构造函数中保存键名
     self.cache = {}
 end)
 
----@param bufnr number
----@param key string|number
----@return any
-function Cache:get(bufnr, key)
-    if not self.cache[bufnr] then
-        return nil
+local function navigateCache(cache, values, createIfMissing, setValue)
+    local tableRef = cache
+    local searchSteps = #values
+    for i = 1, searchSteps - 1 do
+        local key = values[i]
+        if tableRef[key] == nil then
+            if createIfMissing then
+                tableRef[key] = {}
+            else
+                return nil
+            end
+        end
+        tableRef = tableRef[key]
     end
-    return self.cache[bufnr][key]
-end
 
----@param bufnr number
----@param key string|number
----@param value any
-function Cache:set(bufnr, key, value)
-    if not self.cache[bufnr] then
-        self.cache[bufnr] = {}
+    if setValue then
+        tableRef[values[searchSteps]] = setValue
+        return setValue
+    else
+        return tableRef[values[searchSteps]]
     end
-    self.cache[bufnr][key] = value
 end
 
----@param bufnr number
----@param key string|number
----@return boolean
-function Cache:has(bufnr, key)
-    return self.cache[bufnr] and self.cache[bufnr][key]
+function Cache:get(...)
+    local values = { ... }
+    if #values ~= #self.keys then
+        error("The number of keys passed to get() must match the number of keys passed to the constructor")
+    end
+    return navigateCache(self.cache, values, false)
 end
 
----@param bufnr number
-function Cache:clear(bufnr)
-    self.cache[bufnr] = {}
+function Cache:set(...)
+    local values = { ... }
+    if #values ~= #self.keys + 1 then
+        error("The number of keys passed to set() must be one more than the number of keys passed to the constructor")
+    end
+    local value = table.remove(values) -- 将最后一个参数作为要设置的值
+    navigateCache(self.cache, values, true, value)
+end
+
+function Cache:has(...)
+    local values = { ... }
+    if #values ~= #self.keys then
+        error("The number of keys passed to has() must match the number of keys passed to the constructor")
+    end
+    return navigateCache(self.cache, values, false) ~= nil
+end
+
+function Cache:clear(...)
+    local values = { ... }
+    if #values == 0 then
+        self.cache = {}
+    else
+        navigateCache(self.cache, values, false, {})
+    end
 end
 
 return Cache
