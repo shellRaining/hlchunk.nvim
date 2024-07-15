681b4b1028571b798578244fd495221877ddaa78
xiaomo <xiaom0@tom.com>
2024-07-15 18:22:25 +0800

    Add support for css

---
+++ lua/hlchunk/utils/ts_node_type/css.lua
@@ -0,0 +1,11 @@
+return {
+    ["at_keyword"] = true,
+    ["at_rule"] = true,
+    ["charset_statement"] = true,
+    ["import_statement"] = true,
+    ["keyframes_statement"] = true,
+    ["media_statement"] = true,
+    ["namespace_statement"] = true,
+    ["rule_set"] = true,
+    ["supports_statement"] = true,
+}
