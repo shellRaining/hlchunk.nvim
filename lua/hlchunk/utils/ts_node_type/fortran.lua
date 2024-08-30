51443c0ebeb10de841770e8c0d02c72bb738f43e
shion <shimizu.shion819@gmail.com>
2024-08-30 15:30:08 +0900

    Add support for Fortran
    - Lua file for Fortran support is added

---
+++ lua/hlchunk/utils/ts_node_type/fortran.lua
@@ -0,0 +1,18 @@
+return {
+    ["interface"] = "interface",
+    block_construct = "block_construct",
+    derived_type_definition = "derived_type_definition",
+    derived_type_procedures = "derived_type_procedures",
+    do_loop_statement = "do_loop_statement",
+    else_clause = "else_clause",
+    elseif_clause = "elseif_clause",
+    ["function"] = "function",
+    function_statement = "function_statement",
+    if_statement = "if_statement",
+    included_items = "included_items",
+    ["subroutine"] = "subroutine",
+    assignment_statement = "assignment_statement",
+    subroutine_call = "subroutine_call",
+    subroutine_statement = "subroutine_statement",
+    variable_declaration = "variable_declaration",
+}
